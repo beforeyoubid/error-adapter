77639d915698613051553b221f819a2c79111aab
Alice <alice@beforeyoubid.com.au>
2021-11-25 15:25:23 +1100

    added typing for withSentry wrapper

--- src/sentry/serverless/index.ts
+++ src/sentry/serverless/index.ts
@@ -1,15 +1,20 @@
-let serverlessSentry = require('serverless-sentry-lib');
+import serverlessSentry, { WithSentryOptions, Handler } from 'serverless-sentry-lib';
 import { Sentry } from '../node';
+
+const isArgOptions = (arg: WithSentryOptions | Handler<any, any>): arg is WithSentryOptions => typeof arg === 'object';
+const isArgHandler = (arg: WithSentryOptions | Handler<any, any>): arg is Handler<any, any> => typeof arg === 'function';
 
 /**
  * Wrap the serverless-sentry-lib function call to default to using own Sentry client unless custom options provided
  */
-const withSentry = (arg1: object, arg2?: any) => {
-  if (typeof arg1 === 'object') {
+function withSentry(options: WithSentryOptions, handler: Handler<any, any>): Handler<any, any>;
+function withSentry(handler: Handler<any, any>): Handler<any, any>;
+function withSentry (arg1: WithSentryOptions | Handler<any, any>, arg2?: Handler<any, any>): Handler<any, any> {
+  if (isArgOptions(arg1) && arg2 !== undefined && isArgHandler(arg2)) {
     // custom options passed in, include in callback
     // this allows the use of serverless-sentry-lib to handle custom options
     return serverlessSentry(arg1, arg2);
-  } else if (typeof arg1 === 'function') {
+  } else if (isArgHandler(arg1)) {
     // no custom options, intialise and pass in Sentry client, arg1 is the callback function
     Sentry.initialise();
     return serverlessSentry({ sentry: Sentry }, arg1);
